a9846f8747faaff860e152cf6fff2e30ae7cdfc7
Michael Northorp <mike.northorp@gmail.com>
2013-07-25 01:25:36 -0700

    Start memory working and started get_memory

--- binarybuddies.c
+++ binarybuddies.c
@@ -10,97 +10,81 @@ int freeMemory;
 int usedMemory;
 
 //base pointer returned by malloc
+void *mallocPointer;
+
+// Base pointer to our top level header
 void *basePointer;
 
-
+// Header struct containing memory size, if it is used, pointer to the memory address of header
+// and the a
 typedef struct
 {
-    //integer for size of memory in node
-    short memSize;
-    //integer to act aqs a flag to designate whether or not the memory is used. 1 for used, 0 for free
-    char isUsed;
-    // Pointer to the memory address of this node
+    // Total header size = 16bytes after padding
+    // Short for size of memory up to 4294967296 bytes - 4bytes
+    int memSize;
+    // Flag for if memory is used or not - 2byte
+    short isUsed;
+    // Storage of buddy number - 2byte
+    short buddy;
+    // Pointer to the memory address of this node - 2bytes
     void *memPointer;
 } buddyNode;
 
+// Initializes a single malloc call to a passed in size in bytes, and stores a global
+// pointer to the root header containing the root block.
 int start_memory(int size)
 {
-    //if structs are used to keep track of mem blocks this malloc will have to account for them as well
-    if (basePointer = malloc(size))
+    // Try to allocate the size taken in by the start memory function
+    if ((mallocPointer = malloc(size)))
     {
-        //get size into a global variable
+        // Set size to the global variable total memory
         totalMemory = size;
-        //tracks the number of possible buddies
-        int numberOfBuddies = 0;
-        //while loop to get the number of possible buddies
-        size = size - (sizeof(buddyNode));
-        usedMemory += (sizeof(buddyNode));
-        while (size / 2 != 4)
-        {
-            numberOfBuddies++;
-            size = size - (sizeof(buddyNode) * 2);
-            size = size / 2;
-            usedMemory += (sizeof(buddyNode) * 2);
-        }
 
-        freeMemory = totalMemory - usedMemory;
+        // Set up base buddyNode struct pointer to the returned malloc address
+        buddyNode *root = (buddyNode *)mallocPointer;
 
-        //include a data structure here
-        void *buddies[numberOfBuddies * 2 + 1];
+        // Set initial free space (total minus header)
+        root->memSize = (totalMemory - sizeof(buddyNode));
+        // Set not used
+        root->isUsed = 0;
+        // Set buddy number
+        root->buddy = 0;
+        // Set the address to the very start of root block
+        root->memPointer = mallocPointer;
 
-        //base partition
-        buddyNode *base = basePointer;
+        // Set global base pointer to the buddyNode root
+        basePointer = (void *)root;
 
-        base.memSize = freeMemory;
-        base.isUsed = 0;
-        base->memPointer = basePointer;
-
-        buddies[0] = (void *)basePointer;
-
-        //set up all the structs were going to use and assign them memory from the malloc call
-        int i;
-        int j;
-        double count;
-        int k = 1;
-        for (i = 1; i < numberOfBuddies; i++)
-        {
-            // Figure out how many sets of buddys to make
-            count = pow(2.0, (double)i) / 2;
-
-
-            for (j = 0; j < count; j++)
-            {
-                int blockSize = freeMemory / (count*2);
-                int buddySize = sizeof(buddyNode);
-
-                // Set up first buddy
-                buddyNode node1 = basePointer + ((j*2) * (blockSize + buddySize));
-                node1.memSize = blockSize;
-                node1.isUsed = 0;
-                node1->memPointer = basePointer + ((j*2) * (blockSize + buddySize));
-
-                // Set up second buddy
-                buddyNode node2 = basePointer + (((j*2)+1) * (blockSize + buddySize));
-                node2.memSize = blockSize;
-                node2.isUsed = 0;
-                node2->memPointer = basePointer + (((j*2)+1) * (blockSize + buddySize));
-
-                // Put buddy nodes into array with their header and mempointers and size
-				int buddy1Pos = 2*k-1;
-				int buddy2Pos = 2*k;
-                buddies[buddy1Pos] = (void *)node1;
-                buddies[buddy2Pos] = (void *)node2;
-                k++;
-            }
-        }
-        //set global pointer to point to array
-        arrayPointer = buddies;
-
-
+        // Return 1 on sucess
         return 1;
     }
+    // Return 0 on fail
     else
     {
+        printf("Could not allocate memory\n");
         return 0;
     }
+}
+
+// This mallocs
+void *get_memory( int size )
+{
+	//declare buddy node to be created
+	buddyNode node;
+}
+
+int main( int argc, char **argv )
+{
+    printf("Test\n");
+    start_memory(256);
+    printf("End start mem\n");
+    printf("Print out malloc pointer for initial check\n");
+    printf("%p\n", mallocPointer);
+    printf("Print root address, isUsed, memsize and buddy\n");
+    printf("%p\n", ((buddyNode *)basePointer)->memPointer);
+    printf("%d\n", ((buddyNode *)basePointer)->isUsed);
+    printf("%d\n", ((buddyNode *)basePointer)->memSize);
+    printf("%d\n", ((buddyNode *)basePointer)->buddy);
+
+    return 1;
 }
