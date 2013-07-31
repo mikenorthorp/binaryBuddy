ce076bab015be5bd3923655ec34501d83f4d33c4
FusionAmoeba <robert@cs.dal.ca>
2013-07-31 10:19:34 -0300

    This is the new one

--- binarybuddies.c
+++ binarybuddies.c
@@ -1,82 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
-//global vars
-//http://simplestcodings.blogspot.ca/2012/10/abstract-binary-search-tree.html - binary tree we could use
-BinaryTree tree;
-int memoryAvaliable;
 
-//where the only malloc call is permitted
+//global vars
+int totalMemory; //this is the total memory that malloc gets in start_memory minus the struct memory
+
+int freeMemory;
+
+int usedMemory;
+
+//base pointer returned by malloc
+void *basePointer;
+//pointer to array
+void *arrayPointer;
+
+
+typedef struct
+{
+    //integer for size of memory in node
+    int memSize;
+    //integer to act aqs a flag to designate whether or not the memory is used. 1 for used, 0 for free
+    char isUsed;
+    //char to designate which node is the buddy of this one; i.e: 1 and 1 are buddies, 2 and 2 are buddies etc...
+    char buddy;
+} buddyNode;
+
 int start_memory(int size)
 {
-	//if structs are used to keep track of mem blocks this malloc will have to account for them as well
-	if(malloc(size))
-	{
-		//find a way to keep track of mem blocks given to us by malloc, a struct maybe?
+    //if structs are used to keep track of mem blocks this malloc will have to account for them as well
+    if (basePointer = malloc(size))
+    {
+        //get size into a global variable
+        totalMemory = size;
+        //tracks the number of possible buddies
+        int numberOfBuddies = 0;
+        //while loop to get hte nu,mber of possible buddies
+        size = size - (sizeOf(buddyNode));
+        usedMemory += (sizeOf(buddyNode));
+        while (size / 2 != 4)
+        {
+            numberOfBuddies++;
+            size = size - (sizeOf(buddyNode) * 2);
+            size = size / 2;
+            usedMemory += (sizeOf(buddyNode) * 2);
+        }
 
-		memoryAvaliable = size;
-		return 1;
-	}
+        freeMemory = totalMemory - usedMemory;
 
-	else
-		reutrn 0;
-}
+        //include a data structure here
+        void buddies[numberOfBuddies * 2 + 1];
 
-void *get_memory(int size)
-{
-	Node currentNodeInBinaryTree = tree.root();
+        //base partition
+        buddyNode base = basePointer;
 
-	if(memoryAvaliable >= size)
-	{
-		while(currentNodeInBinaryTree.size() - size > currentNodeInBinaryTree.size()/2)
-		{
-			Tree.add(currentNodeInBinaryTree/2);
-			currentNodeInBinaryTree = tree.next();
-		}
+        base.memSize = freeMemory;
+        base.isUsed = 0;
+        base.buddy = 0;
 
-		memoryAvaliable -= size;
+        buddies[0] = (void*)basePointer;
 
-		return (void*)currentNodeInBinaryTree;
-	}
-	else
-		return NULL;
-
-}
-
-void release_memory(void *p)
-{
-	//mark the nodes pointed to by tyhe pointer as having a value of 0 (empty)
-	//probably going to need to cast it to 'Node'
-	Node currentNodeInBinaryTree = p;
-	//add the total memory freed back to the avaliable memory
-	memoryAvaliable += currentNodeInBinaryTree;
-
-	currentNodeInBinaryTree = 0;
-
-	//check to see if we can reclaim the buddies by checking the parent node
-	currentNodeInBinaryTree = currentNodeInBinaryTree.parent();
-
-	if(currentNodeInBinaryTree.leftNode() == 0 && currentNodeInBinaryTree.rightNode()==0)
-	{
-		currentNodeInBinaryTree.deleteLeftNode();
-		currentNodeInBinaryTree.deleteRightNode();
-	}
-}
-
-void *grow_memory(int size, void *p)
-{
-	//find out what mem block p points to
-	//memory in mem block = memory in mem block + size
-}
+        //set up all the structs were going to use and assign them memory from the malloc call
+        int i = 0
+                for (i = 0; i <= numberOfBuddies; i++)
+        {
+            buddies[i * 2] = buddyNode
+                             buddies[i * 2 + 1] = buddyNode
+        }
+        //set global pointer to point to array
+        arrayPointer = buddies;
 
 
 
-//tear down method
-void end_memory()
-{
-	
-	//reclaim all mem blocks were keeping track of
-	//break down binary tree
-	//free the memory we claimed
-	free(sizeOf(mem blocks));
 
+
+        return 1;
+    }
+
+    else
+        reutrn 0;
 }
